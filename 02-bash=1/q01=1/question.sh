cc527265888d6a1da508bdb23c0694bcba37d608
Miguel Jiménez <lumjimenezgo@unal.edu.co>
2020-03-09 15:35:53 -0500

    Notas 2

--- 02-bash=1/q01=1/question.sh
+++ 02-bash=1/q01=1/question.sh
@@ -5,14 +5,14 @@
 ##  Usando los archivos `data1.csv`, `data2.csv`, `data3.csv`, escriba en el
 ##  archivo `script.sh`  un programa en Bash que imprima en pantalla
 ##  la siguiente salida por linea:
-## 
+##
 ##  * El nombre del archivo.
 ##  * El número de la línea procesada.
 ##  * La letra de la primera columna del archivo.
-##  * La cadena de tres letras y el valor asociado de la columna 2 del archivo original. 
+##  * La cadena de tres letras y el valor asociado de la columna 2 del archivo original.
 ##
 ##  Note que se genera una línea de salida por cada cadena de tres letras.
-##   
+##
 ##  Rta/
 ##
 ##  data1.csv,1,E,jjj:3
@@ -22,6 +22,7 @@
 ##  data3.csv,3,B,ddd:2
 ##
 ##  >>> Escriba su codigo a partir de este punto <<<
+##
 Archivos=$(ls *.csv)
 for file in ${Archivos[*]}; do 
     if [ -f 1.csv ]; then
